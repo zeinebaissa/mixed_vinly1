604b994c502968ba623c1318e0551c02bfac1177
Abdallah Ben Salem <abenssalem32@gmail.com>
2024-03-09 17:15:14 +0100

    new changes

--- assets/controllers/song-controls_controller.js
+++ assets/controllers/song-controls_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
-
+import axios from 'axios';
 /*
  * This is an example Stimulus controller!
  *
@@ -10,8 +10,16 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
+    static values = {
+        infoUrl: String
+    }
     play(event) {
         event.preventDefault();
-        console.log('Playing!');
+
+        axios.get(this.infoUrlValue)
+        .then((response)=>{
+            const audio = new Audio(response.data.url);
+            audio.play();
+        });
     }
 }
